0570cdc90300c229baf7a57259661b572ced9763
DaChickenGamer <chickencodes@gmail.com>
2024-06-11 20:52:25 -0400

    Rays Solution To 0001

--- 0001 | Two Sum/Rays Soluton.cs
+++ 0001 | Two Sum/Rays Soluton.cs
@@ -1,6 +1,27 @@
-namespace DefaultNamespace;
+public class Rays_Solution {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int currentTarget = 0;
+        bool runProgram = true;
+        
+        while (runProgram)
+        {
+            for(int i = 0; i < nums.Length; i++){
+                for(int n = 0; n < nums.Length; n++){
+                    if (i != n){
+                        currentTarget = nums[i] + nums[n];
 
-public class Rays_Soluton
-{
-    
+                        if (currentTarget == target)
+                        {
+                            int[] answer = {i, n};
+                            return answer;
+                        }
+                    }
+                }
+            }
+			
+        }
+		
+        return null;
+    }
 }
