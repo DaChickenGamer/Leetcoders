ccbed30a8be3c6397555a816cd652a90328c4e8f
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2024-06-11 21:52:20 -0400

    Update Diegos Solution.cs

--- 0001 | Two Sum/Diegos Solution.cs
+++ 0001 | Two Sum/Diegos Solution.cs
@@ -1,6 +1,36 @@
-namespace DefaultNamespace;
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int solution = -2;
+        int num1 = 0;
+        int num2 = 0;
+        int arrayLength = nums.Length;
 
-public class Diegos_Solution
-{
-    
+        while((solution != target))
+        {
+            if(num2 > (arrayLength - 1))
+            {
+                Console.WriteLine("Does not Exist");
+                return null;
+            }
+            else if(num1 <= (arrayLength - 1))
+            {
+                if (!(num1 == num2))
+                    solution = nums[num1] + nums[num2];
+                if((solution != target))
+                {
+                    num1++;
+                }
+            }
+            else if(num1 > (arrayLength - 1)){
+                num1 = 0;
+                num2++;
+            }
+        }
+
+        int[] myNumbers = {num1,num2};
+        //int[] myNumbers = {arrayLength};
+
+        return myNumbers;
+
+    }
 }
